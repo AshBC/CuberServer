67a147a9cd83b8c2527ed7ddbfdebabc9097bb9f
Ash Conery <aconery@gmail.com>
2021-03-23 16:33:04 -0700

    first commit to cube server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require ('express');
+const morgan = require ('morgan');
+const app = express ();
+const cors = require ('cors');
+const siteRoutes= require ('./routes/siteRoutes');
+const PORT = 3001;
+
+app.use(cors());
+app.use(morgan());
+app.use(express.static('public'));
+app.use(express.json());
+app.use('/videos', siteRoutes);
+
+app.get('/', (req,res) => {
+    res.json({
+        greeting: 'welcome to Cube api',
+    });
+});
+app.listen(PORT, console.log(`listening at: http://localhost:${PORT}`));
