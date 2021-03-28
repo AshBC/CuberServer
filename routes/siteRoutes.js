a4e06427553f7fa4c415c0510780539011dd5647
Ash Conery <aconery@gmail.com>
2021-03-28 14:16:22 -0700

    clean up of formatting for submission

--- routes/siteRoutes.js
+++ routes/siteRoutes.js
@@ -1,77 +1,58 @@
-const express = require('express');
-const {v4: uuidv4} = require ('uuid');
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 const router = express.Router();
-const fs = require('fs');
-const { POINT_CONVERSION_HYBRID } = require('constants');
+const fs = require("fs");
+const { POINT_CONVERSION_HYBRID } = require("constants");
 
 function loadVideoData() {
-    return fs.readFileSync("./data/videos.json", "utf8");
- }
-
+  return fs.readFileSync("./data/videos.json", "utf8");
+}
 
 router.get("/", (req, res) => {
-    console.log(req);
-    const VList = JSON.parse(loadVideoData());
-    const videosMapped = VList.map((video) => {
-        return {
-            id: video.id,
-            title: video.title,
-            channel: video.channel,
-            image: video.image,
-        };
-    });
-    res.json(videosMapped);
+  console.log(req);
+  const VList = JSON.parse(loadVideoData());
+  const videosMapped = VList.map((video) => {
+    return {
+      id: video.id,
+      title: video.title,
+      channel: video.channel,
+      image: video.image,
+    };
+  });
+  res.json(videosMapped);
 });
 
-router.get("/:id", (req,res) => {
-    const VList = JSON.parse(loadVideoData());
-    const foundVideoIndex = VList.findIndex((video) => {
-        return video.id === req.params.id;
-    });
-    res.json(VList[foundVideoIndex]);
+router.get("/:id", (req, res) => {
+  const VList = JSON.parse(loadVideoData());
+  const foundVideoIndex = VList.findIndex((video) => {
+    return video.id === req.params.id;
+  });
+  res.json(VList[foundVideoIndex]);
 });
-
-// router.patch("/:id/likes", (req, res) => {
-//         const VList = JSON.parse(loadVideoData());
-//         console.log("videos patch");
-//         const editLikes = {
-//             likes: req.body.likes,
-//         };
-//         const filterItems = VList.filter((item) => item.id === req.params.id);
-//         filterItems.push(editLikes);
-
-//         console.log(filterItems);
-//         fs.writeFileSync("./data/videos.json", JSON.stringify(filterItems));
-
-//         res.json({
-//             message: "posted new likes",
-//         });
-// });
 
 router.post("/", (req, res) => {
-    if (req.body.name === "" && req.body.description === "") {
-        res.status(422).send("please enter a name and description");
-    }else {
-        const VList = JSON.parse(loadVideoData());
-        const newVideo = {
-            id: uuidv4(),
-            title: req.body.name,
-            channel: req.body.channel,
-            description: req.body.description,
-            timestamp: req.body.timestamp,
-            video: req.body.video,
-            image: "https://cubes-server.herokuapp.com/imgs/floatingCube.jpg",
-            comments: [],
-        };
-        VList.push(newVideo);
-        fs.writeFileSync("./data/videos.json", JSON.stringify(VList));
+  if (req.body.name === "" && req.body.description === "") {
+    res.status(422).send("please enter a name and description");
+  } else {
+    const VList = JSON.parse(loadVideoData());
+    const newVideo = {
+      id: uuidv4(),
+      title: req.body.name,
+      channel: req.body.channel,
+      description: req.body.description,
+      timestamp: req.body.timestamp,
+      video: req.body.video,
+      image: "https://cubes-server.herokuapp.com/imgs/floatingCube.jpg",
+      comments: [],
+    };
+    VList.push(newVideo);
+    fs.writeFileSync("./data/videos.json", JSON.stringify(VList));
 
-        res.json({
-            message: "posted to videos endpoint",
-            videoPosted: newVideo,
-        });
-    }
+    res.json({
+      message: "posted to videos endpoint",
+      videoPosted: newVideo,
+    });
+  }
 });
 
-
-module.exports= router;
+module.exports = router;
